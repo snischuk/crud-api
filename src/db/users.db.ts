2802ff00ce4b9240d0c493d2e8aaf147bf6ac8b5
Max Snischuk <m.snischuk@gmail.com>
2025-11-09 22:05:08 +0200

    feat: add in-memory database

---
+++ src/db/users.db.ts
@@ -0,0 +1,34 @@
+import type { User } from '../models/user.model.ts';
+
+const usersDatabase: User[] = [];
+
+export const getAllUsers = async (): Promise<User[]> => [...usersDatabase];
+
+export const getUserById = async (userId: string): Promise<User | undefined> =>
+  usersDatabase.find((user) => user.id === userId);
+
+export const createUser = async (userData: User): Promise<User> => {
+  usersDatabase.push(userData);
+  return userData;
+};
+
+export const updateUser = async (
+  userId: string,
+  userData: User,
+): Promise<User | null> => {
+  const index = usersDatabase.findIndex((user) => user.id === userId);
+  if (index === -1) return null;
+
+  usersDatabase[index] = { ...userData, id: userId };
+  return usersDatabase[index];
+};
+
+export const deleteUser = async (userId: string): Promise<boolean> => {
+  const indexOfExistingUser = usersDatabase.findIndex(
+    (user) => user.id === userId,
+  );
+  if (indexOfExistingUser === -1) return false;
+
+  usersDatabase.splice(indexOfExistingUser, 1);
+  return true;
+};
