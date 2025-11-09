0f57063951bae74e5a439768ac9779c59bdbae37
Max Snischuk <m.snischuk@gmail.com>
2025-11-09 20:12:31 +0200

    feat: create basic http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log(`Server running in dev mode on port ${process.env.PORT || 4444}`);
+import { createServer } from 'node:http';
+// import { usersController } from './controllers/users.controller.ts';
+import { loadEnv } from './utils/loadEnv.ts';
+
+await loadEnv();
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
+
+const server = createServer();
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
